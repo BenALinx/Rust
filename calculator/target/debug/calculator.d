b731f609fe444092b15a46bf170759ddc637a026
Benjamin <archardbendev@gmail.com>
2025-03-01 13:26:31 +0000

    Added Calculator Project

---
+++ calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+/home/benjamin/Code/Repos/Rust/calculator/target/debug/calculator: /home/benjamin/Code/Repos/Rust/calculator/src/main.rs
---
+++ calculator/target/debug/deps/calculator-eab15543d6e123fa.d
@@ -0,0 +1,5 @@
+/home/benjamin/Code/Repos/Rust/calculator/target/debug/deps/calculator-eab15543d6e123fa: src/main.rs
+
+/home/benjamin/Code/Repos/Rust/calculator/target/debug/deps/calculator-eab15543d6e123fa.d: src/main.rs
+
+src/main.rs:
